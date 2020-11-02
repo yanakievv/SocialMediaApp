3c02c19eab26a2ac6b10ab15bf56faace317ffd9
yanakievv <vl.yanakiev@abv.bg>
2020-11-02 18:25:46 +0200

    Optimised image loading and jumping from others posts to own. Removed "App.currentProfile", because it was unreliable and in place of it implemented shared preferences.

--- app/src/main/java/com/example/socialmediaappv2/profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/socialmediaappv2/profile/EditProfileFragment.kt
@@ -19,6 +19,7 @@ private const val ARG_PARAM1 = "uId"
 
 class EditProfileFragment : Fragment(), com.example.socialmediaappv2.contract.Contract.ProfileView {
     private var uId: String? = null
+
     @Suppress("RegExpSingleCharAlternation", "RegExpRedundantEscape")
     private var regexDate: Regex = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})\$|^(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))\$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})\$".toRegex()
 
